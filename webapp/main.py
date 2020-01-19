3b84a158ea84822b69b11b8184be45a2eaf9a1bb
zolamb <zolamb@ucsc.edu>
2020-01-19 01:40:47 -0800

    Updated main

--- webapp/main.py
+++ webapp/main.py
@@ -2,17 +2,35 @@ from flask import Flask, render_template, jsonify
 from pymongo import *
 from passwords import *
 from twitter import *
-#import json
+import datetime
 
-#client = MongoClient("mongodb+srv://zolamb:<password>@scfire-6siv5.gcp.mongodb.net/test?retryWrites=true&w=majority")
-#db = client.SCFire
+def fireResult(tweet):
+    if("FIRE," in tweet["text"]):
+        return True
+    else:
+        return False
+
+def parseTweet(tweet):
+    id_str = tweet["id_str"]
+    text = tweet["text"]
+    arr = text.split(",")
+    arr.pop(0)
+    arr2 = ",".join(arr)
+    arr3 = arr2.split(" (")
+    address = arr3[0][1:]
+    dt = arr3[1].split(" )")[0]
+    actual_date = datetime.datetime.strptime(dt, "%m/%d/%Y %I:%M:%S %p")
+    actual_date_str = actual_date.strftime("%m/%d/%Y %H:%M:%S")
+    ts = actual_date.timestamp()
+
+    return({"id": id_str, "unixtime": ts, "address": address, "timestring": actual_date_str})
 
 app = Flask(__name__)
 
 @app.route('/view_endpoint', methods=['GET'])
 def view_endpoint():
 
-    client = MongoClient("mongodb+srv://lucas:<password>@scfire-6siv5.gcp.mongodb.net/test?retryWrites=true&w=majority")
+    client = MongoClient("mongodb+srv://lucas:pass123@scfire-6siv5.gcp.mongodb.net/test?retryWrites=true&w=majority")
     db = client.SCFire
 
     collection = db.fireData
@@ -20,23 +38,33 @@ def view_endpoint():
     json_dict = dict()
     for i, document in enumerate(cursor):
         json_dict[document['id']] = {"unixtime": document["unixtime"], "timestring":document["timestring"], "address":document["address"]}
-    return jsonify(json_dict)
+    response = jsonify(json_dict)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/check_new_tweet')
 def check_new_tweet():
+    client = MongoClient("mongodb+srv://zolamb:Cloud1755##@scfire-6siv5.gcp.mongodb.net/test?retryWrites=true&w=majority")
+    print(client)
+    db = client.SCFire
     t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, API_KEY_SECRET))
     recent = t.statuses.home_timeline()
     for i in range(20):
-        if(db.fireData.find({"id" : recent[i]['id_str']}).count() == 0):
-            print("------------------HERE-------------------2")
-            print(recent[i]['id_str'])
-            #db.fireData.insert_one(tweet)
+        result = db.fireData.find({"id" : recent[i]['id_str']}).count()
+        # If it doesn't exist in the database
+        if(result == 0):
+            # If the tweet is fire
+            if(fireResult(recent[i])):
+                tweet = parseTweet(recent[i])
+                db.fireData.insert_one(tweet)
         else:
-            break
+            return "Done updating"
+    return "No new tweets"
 
 @app.route('/')
 def home():
     return "home"
+
 
 
 if __name__ == '__main__':
