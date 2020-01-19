381cc48245405aa62fd2bbe9938fceb6ebb8d73d
Lucas B <lucasbattelle@gmail.com>
2020-01-18 20:22:56 -0800

    somestuff changed

--- webapp/main.py
+++ webapp/main.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
-from setup_database import get_fires
+from pymongo import MongoClient
+from passwords import *
+from twitter import *
+
+client = MongoClient("mongodb+srv://zolamb:"+ MONGODB_PASSWORD +"@scfire-6siv5.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = client.SCFire
 
 app = Flask(__name__)
 
+@app.route('/check_new_tweet')
+def check_new_tweet():
+    t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, API_KEY_SECRET))
+    recent = t.statuses.home_timeline()
+    
+    find_result = db.fireData.find({"id" : recent[1]['id_str']})
+
+    
+    return recent[1]['id_str']
+    
 
 @app.route('/')
 def home():
-    return get_fires()
+    return "home"
 
 
 if __name__ == '__main__':
